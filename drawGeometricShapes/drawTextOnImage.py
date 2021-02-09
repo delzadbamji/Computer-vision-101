cd19d197b819794ab4a82018824f3114fc2c0c1a
Delzad Bamji <delzadbamji@gmail.com>
2021-02-09 08:20:08 -0500

    Update drawTextOnImage.py

--- drawGeometricShapes/drawTextOnImage.py
+++ drawGeometricShapes/drawTextOnImage.py
@@ -2,7 +2,7 @@ import cv2
 
 
 # ---------------------text on image----------------
-img = cv2.imread('C:\\Users\\Delzad\\PycharmProjects\\openCvTutorials\\hackathon.png', 1)
+img = cv2.imread('C:\\fakepath\\hackathon.png', 1)
 
 
 # params:
